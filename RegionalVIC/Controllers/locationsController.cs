964fca6ced18dd590a93a6bd0f95835624a80a47
Sam <kasaluoqi@gmail.com>
2019-04-03 19:57:37 +1100

    v0.3.2 Migrate to .NET core and add chart.js

---
+++ RegionalVIC/Controllers/locationsController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Assignment.Controllers
+{
+    public class locationsController : Controller
+    {
+        // GET: locations
+        public ActionResult Index()
+        {
+            return View();
+        }
+        
+    }
+}
